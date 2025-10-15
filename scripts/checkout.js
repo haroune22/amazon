df4b9425bf0a95107832c0615daa9a17258f541c
haroune22 <harounehamenniche123@gmail.com>
2025-10-15 11:00:27 +0100

    external libraries

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
-import { formatCurrency } from "../data/utils/money.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHtml = "";
 
